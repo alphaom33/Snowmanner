8b07c5cc85ae54cfa4dfacd75b7be156be0ed8be
Matthew Borchert <alphaom34@gmail.com>
2024-08-17 19:24:00 -0500

    door

---
+++ Assets/PuzzleElements/DoorController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public List<ButtonController> buttons;
+    public bool last;
+
+    private Collider myCollider;
+
+    public float speed;
+    public Transform model;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCollider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool ack = true;
+        foreach (var button in buttons)
+        {
+            ack &= !button.pressed;
+        }
+
+        if (last != ack)
+        {
+            StartCoroutine(DoorSwing());
+        }
+        myCollider.enabled = ack;
+        last = ack;
+    }
+
+    IEnumerator DoorSwing()
+    {
+        float start;
+        float end;
+        if (!last)
+        {
+            start = 0;
+            end = 90;
+        }
+        else
+        {
+            start = 90;
+            end = 0;
+        }
+
+        for (float i = 0; i < 1; i += Time.deltaTime * speed)
+        {
+            model.rotation = Quaternion.Euler(model.eulerAngles.x, Utils.EaseOutInSine(start, end, i), model.eulerAngles.z);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
